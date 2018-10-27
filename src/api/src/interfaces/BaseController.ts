87fec24ff262276fd18941729fbdf4592d356721
Tanner Goins <tannergz28@gmail.com>
2018-10-27 17:30:42 -0500

    Updated to allow methods to use decorator parameters

--- src/api/src/interfaces/BaseController.ts
+++ src/api/src/interfaces/BaseController.ts
@@ -1,9 +1,7 @@
-import { Request, Response } from 'express'
-
 export interface BaseController<T, IDColumnType> {
-  find: (request: Request, response: Response) => Promise<T[]>
+  find: (...args: any[]) => Promise<T[]>
   findById: (id: IDColumnType, ...args: any[]) => Promise<T | undefined>
   create: (item: T, ...args: any[]) => Promise<T | undefined>
-  updateById: (id: IDColumnType, item: T, ...args: any[]) => Promise<T>
-  deleteById: (id: IDColumnType, request: Request, response: Response) => Promise<T | undefined>
+  updateById: (id: IDColumnType, item: T, ...args: any[]) => Promise<T | undefined>
+  deleteById: (id: IDColumnType, ...args: any[]) => Promise<T | undefined>
 }
