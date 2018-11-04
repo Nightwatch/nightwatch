a6c3b24ed6ec85f3e4330183769ab446d8a4ced2
Tanner Goins <tannergz28@gmail.com>
2018-11-03 20:41:09 -0500

    Rename files to be lowercase

--- src/api/src/interfaces/BaseService.ts
+++
@@ -1,7 +0,0 @@
-export interface BaseService<T, IDColumnType> {
-  find: () => Promise<T[]>
-  findById: (id: IDColumnType) => Promise<T | undefined>
-  create: (model: T) => Promise<T>
-  update: (id: IDColumnType, model: T) => Promise<T>
-  delete: (id: IDColumnType) => Promise<T | undefined>
-}
