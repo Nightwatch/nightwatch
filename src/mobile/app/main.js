a5c4ae25e2a12e1d261a5a559e91d9456e8589fb
Tanner Goins <tannergz28@gmail.com>
2018-11-11 13:40:15 -0600

    Use devtools

--- src/mobile/app/main.js
+++ src/mobile/app/main.js
@@ -1,14 +1,21 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
+import devTools from '@vue/devtools'
 import {
   RadSideDrawer
 } from 'nativescript-ui-sidedrawer'
 
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+const isProduction = TNS_ENV === 'production' // eslint-disable-line
 
 Vue.registerElement('RadSideDrawer', () => RadSideDrawer)
 
 new Vue({
   render: h => h('frame', [h(App)])
 }).$start()
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = isProduction
+
+if (!isProduction) {
+  devTools.init(Vue)
+}
