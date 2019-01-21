4eb037eb469f844adbb6e5ec775b582ede24d42a
Tanner Goins <tannergz28@gmail.com>
2019-01-20 22:51:36 -0600

    Add .data

--- src/bot/commands/search/gif.ts
+++ src/bot/commands/search/gif.ts
@@ -38,12 +38,12 @@ export default class GifCommand extends Command {
     try {
       const response = await axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${config.optional.giphyApiKey}&tag=${encodeURIComponent(search)}`)
 
-      if (!response.data.url) {
+      if (!response.data.data.url) {
         return msg.channel.send('Nothing found!')
       }
 
       const embed = new MessageEmbed()
-          .setImage(`${response.data.url}`)
+          .setImage(`${response.data.data.url}`)
           .setAuthor(`${msg.author.tag}`, msg.author.displayAvatarURL())
           .setColor('#0066CC')
 
