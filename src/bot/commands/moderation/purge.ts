e78b0b55748a4909d9c2b3a57af5cb48307e5661
Tanner Goins <tannergz28@gmail.com>
2019-01-28 21:12:09 -0600

    Add purge command

---
+++ src/bot/commands/moderation/purge.ts
@@ -0,0 +1,44 @@
+import { Message, TextChannel } from 'discord.js'
+import { Command, CommandoMessage, CommandoClient } from 'discord.js-commando'
+
+export default class PurgeCommand extends Command {
+  constructor (client: CommandoClient) {
+    super(client, {
+      name: 'purge',
+      group: 'moderation',
+      memberName: 'purge',
+      description: 'Deletes messages from a channel. (Messages must be less than 14 days old)',
+      guildOnly: true,
+      throttling: {
+        usages: 2,
+        duration: 3
+      },
+      args: [
+        {
+          key: 'amount',
+          prompt: 'How many messages should I delete?\n',
+          type: 'integer',
+          max: 100,
+          min: 1
+        }
+      ]
+    })
+  }
+
+  public hasPermission (msg: CommandoMessage): boolean {
+    return (
+      this.client.isOwner(msg.author) ||
+      msg.member.hasPermission('MANAGE_MESSAGES')
+    )
+  }
+
+  public async run (msg: CommandoMessage, args: any): Promise<Message | Message[]> {
+    const amount = args.amount as number
+
+    const textChannel = msg.channel as TextChannel
+
+    await textChannel.bulkDelete(amount)
+
+    return msg.reply('Purge complete.')
+  }
+}
