75fe692b181c57c8604583b24fbe5c5faac084de
Tanner Goins <tannergz28@gmail.com>
2019-01-30 17:02:21 -0600

    Reverted test orm settings

--- src/api/test/index.ts
+++ src/api/test/index.ts
@@ -1,6 +1,22 @@
 import { createConnection } from 'typeorm'
-const testSettings = require('../../../ormconfig.test.json')
 
 export async function createTestDatabaseConnection() {
-  await createConnection(testSettings)
+  await createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '',
+    database: 'nightwatch_test',
+    synchronize: true,
+    logging: false,
+    entities: ['src/db/entity/**/*.ts'],
+    migrations: ['src/db/migrations/**/*.ts'],
+    subscribers: ['src/db/subscribers/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/db/entities',
+      migrationsDir: 'src/db/migrations',
+      subscribersDir: 'src/db/subscribers'
+    }
+  })
 }
