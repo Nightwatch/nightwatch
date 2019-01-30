41b4096bc21cc2df07358c067939d53af6eac957
Tanner Goins <tannergz28@gmail.com>
2019-01-29 19:24:15 -0600

    Fix reaction

--- src/bot/commands/misc/poll.ts
+++ src/bot/commands/misc/poll.ts
@@ -64,7 +64,7 @@ export default class PollCommand extends Command {
     const sentMessage = await textChannel.send(embed) as Message
 
     for (let i = 0; i < result.options.length; i++) {
-      await sentMessage.react(`:${String.fromCharCode(97 + i)}:`)
+      await sentMessage.react(`regional_indicator_${String.fromCharCode(97 + i)}`)
     }
 
     return msg.channel.send(`There's a new poll in #poll! Go check it out.`)
