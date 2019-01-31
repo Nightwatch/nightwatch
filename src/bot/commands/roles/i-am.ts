4b94f06b981adb471c50097aeba28088ecedf78b
Tanner Goins <tannergz28@gmail.com>
2019-01-30 19:24:28 -0600

    Add iam command

---
+++ src/bot/commands/roles/i-am.ts
@@ -0,0 +1,43 @@
+import { Message, Role } from 'discord.js'
+import { Command, CommandoMessage, CommandoClient } from 'discord.js-commando'
+import { GuildService } from '../../services'
+import { GuildSelfAssignableRole } from '../../../db'
+
+export default class IAmRoleCommand extends Command {
+  constructor (client: CommandoClient) {
+    super(client, {
+      name: 'iam',
+      group: 'roles',
+      memberName: 'iam',
+      description: 'Give yourself a role.',
+      guildOnly: true,
+      throttling: {
+        usages: 2,
+        duration: 3
+      },
+      args: [
+        {
+          key: 'role',
+          prompt: 'What role do you want?\n',
+          type: 'role|string'
+        }
+      ]
+    })
+  }
+
+  public async run (msg: CommandoMessage, args: any): Promise<Message | Message[]> {
+    const role: Role = args.role instanceof Role ? args.role : msg.guild.roles.find(x => x.name.toLowerCase() === args.role.toLowerCase().trim())
+
+    if (!role) {
+      return msg.reply(`Could not find a self assignable role named ${args.role}`)
+    }
+
+    try {
+      await msg.member.roles.add(role)
+    } catch {
+      // swallow
+    }
+
+    return msg.reply(`You are now a **${role.name}**!`)
+  }
+}
