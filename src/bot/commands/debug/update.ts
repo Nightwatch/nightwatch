5db19016c171e3d697f43b9a939df1416876392f
Tanner Goins <tannergz28@gmail.com>
2019-02-05 17:56:37 -0600

    Fix path

--- src/bot/commands/debug/update.ts
+++ src/bot/commands/debug/update.ts
@@ -31,7 +31,7 @@ export default class UpdateCommand extends Command {
     const result = await git.pull()
 
     try {
-      const premium = await git.cwd(path.resolve(__dirname, '..', '..', '..', '..', 'src', 'plugins', 'plugin-premium'))
+      const premium = await git.cwd(path.resolve(__dirname, '..', '..', '..', '..', 'src', 'bot', 'plugins', 'plugin-premium'))
         .catch(() => undefined)
 
       if (config.optional && config.optional.premium && config.optional.premium.premiumPluginRepo) {
@@ -45,7 +45,7 @@ export default class UpdateCommand extends Command {
           }
         }
 
-        await git.clone(repo, path.resolve(__dirname, '..', '..', '..', '..', 'src', 'plugins'))
+        await git.clone(repo, path.resolve(__dirname, '..', '..', '..', '..', 'src', 'bot', 'plugins'))
       }
     } catch (err) {
       await msg.channel.send((err as Error).message)
