780377dcac675f3d8eb978703abbb6a8bb2366f3
BuildTools <unconfigured@null.spigotmc.org>
2020-02-25 12:36:22 -0600

    Removed Chef/Vagrant in favor of Docker
    Fixes #230 and #245

--- chef/cookbooks/main/.delivery/build_cookbook/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'build_cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-version '0.1.0'
-chef_version '>= 13.0'
-
-depends 'delivery-truck'
--- chef/cookbooks/main/.delivery/build_cookbook/recipes/smoke.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook:: build_cookbook
-# Recipe:: smoke
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-include_recipe 'delivery-truck::smoke'
