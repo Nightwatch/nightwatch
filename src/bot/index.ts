88c5c4bf58198f12a5a5cff52bbbe91f6947815d
BuildTools <unconfigured@null.spigotmc.org>
2020-02-27 11:54:56 -0600

    Restart bot on unhandled error

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,21 +1,32 @@
 import { container } from './config/inversify'
 import { Types } from '../common'
 import { Bot } from './interfaces'
-import * as Promise from 'bluebird'
 
 /* prettier-ignore */
-const bot = container.get<Bot>(Types.Bot)
-
-Promise.resolve(bot.start()).catch(onError)
 
 process.on('uncaughtException', onError)
 process.on('disconnect', onError)
+
+function start() {
+  const bot = container.get<Bot>(Types.Bot)
+  bot
+    .start()
+    .then(() => {
+      //
+    })
+    .catch(onError)
+}
 
 function onError(error?: Error) {
   if (error) {
     console.error(error)
   }
 
+  // TODO: Make auto restarts work in Docker
   // Will restart automatically
-  process.exit(1)
+  // process.exit(1)
+
+  start()
 }
+
+start()
