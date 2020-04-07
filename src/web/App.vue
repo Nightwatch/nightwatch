bf6c6745eb60d57cf5c8c58993a52929720d731c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 09:34:32 -0500

    Web rewrite

--- src/web/App.vue
+++ src/web/App.vue
@@ -1,28 +1,22 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <Header/>
 
     <router-view></router-view>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from '@/web/components/core/Footer'
+import Header from '@/web/components/core/Header'
+
 export default {
   name: 'App',
+  components: {
+    Footer,
+    Header
+  },
   data () {
     return {
       //
