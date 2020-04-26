746be299655ee467c3845c5669915b5fd7b37c53
BuildTools <unconfigured@null.spigotmc.org>
2020-04-26 16:59:07 -0500

    Refactor

--- src/bot/models/client.ts
+++ src/bot/models/client.ts
@@ -1,8 +1,6 @@
 import { CommandoClient } from 'discord.js-commando'
 import { SongQueue } from './guild'
-import { MusicPlayer } from '../commands/music/music-player'
 
 export class Client extends CommandoClient {
   public songQueues: Map<string, SongQueue> = new Map()
-  public musicPlayer = new MusicPlayer(this)
 }
