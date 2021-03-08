4659d9fb7b20b87ce1c1763911727613175286a8
Tanner Goins <tanner@tannergoins.com>
2021-03-08 16:23:32 -0600

    Remove plugin specific code

--- src/bot/models/client.ts
+++ src/bot/models/client.ts
@@ -1,6 +1,4 @@
 import { CommandoClient } from 'discord.js-commando'
-import { SongQueue } from './guild'
 
 export class Client extends CommandoClient {
-  public songQueues: Map<string, SongQueue> = new Map()
 }
